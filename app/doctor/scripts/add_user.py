cf3dc3c22f424565fbbd9c7433a2839f7f393c13
ttuanho <hhoanhtuann@gmail.com>
2020-06-13 08:48:05 +1000

    update

--- app/doctor/scripts/add_user.py
+++ app/doctor/scripts/add_user.py
@@ -1,8 +1,17 @@
 from faker import Faker
 fake = Faker()
 from core.models import User
-
+import random
+import time
 
 def run(*args):
-    pass
+    for i in range(0, 1000):
+        User.object.create(
+            email=fake.email(),
+            name=.fake.name(),
+            registration_number= None,
+            registration_expiry_date=None,
+            profesion=None,
+            is_staff=False
+        )
 
